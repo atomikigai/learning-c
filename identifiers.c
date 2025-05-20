c12a9984137c9cf420d72f3b685d5c99e0b6df85
Jostick <jostickquiel@proton.me>
2025-05-20 08:30:55 -0500

    identifiers

---
+++ identifiers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    // in c the programmers follow the convetion of using only lower-case letters in identifiers
+    //  symbol_table current_page
+
+    return 0;
+}
