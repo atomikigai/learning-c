641a18fb694cb6710736660425a41491806679bd
Jostick <jostickquil@proton.me>
2025-05-27 09:50:56 -0500

    mejora

---
+++ arithmetic.c
@@ -0,0 +1,10 @@
+// the operator % requires integer, if not the program won't compile.
+// the operator / suprising results when both are integers truncate 1/2values i 0 not 0.5
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
